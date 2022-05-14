c7506c0466ee2eb4cb5be18f182df31fc0b9b340
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-05-14 20:39:54 +0900

    Time: 56 ms (70.77%), Space: 13.9 MB (31.54%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        s = s.replace('IV', ' 4 ')
+        s = s.replace('IX', ' 9 ')
+        s = s.replace('XL', ' 40 ')
+        s = s.replace('XC', ' 90 ')
+        s = s.replace('CD', ' 400 ')
+        s = s.replace('CM', ' 900 ')
+        s = s.replace('I', ' 1 ')
+        s = s.replace('V', ' 5 ')
+        s = s.replace('X', ' 10 ')
+        s = s.replace('L', ' 50 ')
+        s = s.replace('C', ' 100 ')
+        s = s.replace('D', ' 500 ')
+        s = s.replace('M', ' 1000 ')
+        return sum([int(x) for x in s.split()])
