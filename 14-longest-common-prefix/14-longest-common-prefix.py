5652f67999ee5a07a681a3066a8fe0ccc0d1cfe6
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-05-15 19:36:21 +0900

    Time: 37 ms (79.99%), Space: 13.8 MB (88.99%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,10 @@
+class Solution:
+  def longestCommonPrefix(self, strs):
+      prefix=[]
+      num = len(strs)
+      for x in zip(*strs):
+        if len(set(x)) == 1:
+          prefix.append(x[0])
+        else:
+          break
+      return "".join(prefix)
