a341dde11fc94e3606a25ed2ee89313eaa850463
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-05-22 14:55:57 +0900

    Time: 5580 ms (13.20%), Space: 14.9 MB (95.17%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,6 @@
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    checked_map = {}
-    for i in range(len(nums)):
-      remain = target - nums[i]
-      if remain in checked_map:
-        return [i, checked_map[remain]]
-      checked_map[nums[i]] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+          for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+              return [i, j]
