4fa1051e79657783ca5e041484146e45c48e44f2
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-05-22 17:43:23 +0900

    Time: 221 ms (66.24%), Space: 43.3 MB (5.42%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,4 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s = s.reverse()
+        def helper(left, right):
+          if left < right:
+            s[left], s[right] = s[right], s[left]
+            helper(left+1, right -1)
+            
+        helper(0, len(s)-1)
