46aaf519505ff67c61d1da4a8f9029f12dc65f90
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-05-22 16:49:19 +0900

    Time: 61 ms (13.35%), Space: 13.9 MB (24.40%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,14 +1,18 @@
 class Solution:
   def isValid(self, s: str) -> bool:
-    hashMap = {')': '(', '}':'{', ']': '[' }
+    if len(s) < 2: return False
+    mapping = {
+      ')' : '(',
+      '}': '{',
+      ']': '['
+    }
     stack = []
     for i in range(len(s)):
       if s[i] in ['[', '{', '(']:
         stack.append(s[i])
       else:
-        if len(stack) == 0: return False
-        if hashMap[s[i]] != stack.pop():
-            return False
+        if len(stack) == 0 or mapping[s[i]] != stack.pop():
+          return False
     if len(stack) == 0:
         return True
     return False
