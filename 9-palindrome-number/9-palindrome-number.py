c8ea49581b0c7ab573a9a8018be41abbf0acf4ac
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-06-11 01:45:11 +0900

    Time: 100 ms (36.06%), Space: 14 MB (15.67%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,16 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # If x is a negative number it is not a palindrome
-        # If x % 10 = 0, in order for it to be a palindrome the first digit should
-        if x < 0 or (x > 0 and x % 10 == 0):
-          return False
-        
-        reversedNum = 0
-        while x > reversedNum:
-          reversedNum = reversedNum * 10 + x % 10
-          x = x // 10
-          
-        if x == reversedNum or x == reversedNum//10:
-          return True
-        else:
-          return False
+      if x < 0 or (x > 0 and x % 10 == 0):
+        return False
+      
+      reverseNum = 0
+      
+      while x > reverseNum:
+        reverseNum = reverseNum * 10 + x % 10
+        x = x // 10
+      if x == reverseNum or x == reverseNum // 10:
+        return True
+      else:
+        return False
