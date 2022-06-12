2f59a15e3825068721de9cd02ed0208f73e2f0c8
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-06-12 14:16:48 +0900

    Time: 1057 ms (5.01%), Space: 17.8 MB (5.41%) - LeetHub

---
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.py
@@ -0,0 +1,19 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.size = size
+        self.list = []
+
+    def next(self, val: int) -> float:
+      self.list.append(val)
+      temp_sum = 0
+      if self.size == 1:
+        return self.list[len(self.list)-1]
+      if len(self.list) < self.size:
+        for i in range(len(self.list)):
+          temp_sum += self.list[i]
+        return temp_sum / len(self.list)
+      else:
+        for i in range(self.size):
+          temp_sum += self.list[len(self.list) - self.size + i]
+        return temp_sum / self.size
