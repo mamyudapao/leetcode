48bdddaf43ab9cfbbc7340cd77116556570c20d8
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-06-12 11:31:40 +0900

    Time: 82 ms (65.13%), Space: 14 MB (42.63%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,20 +5,20 @@ class ListNode:
 
 class Solution:
   def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-    dummy = ListNode(None)
-    curr = dummy
-    
     carry = 0
+    dummy = ListNode(None)
+    head = dummy
+    
     while l1 or l2 or carry:
-      v1 = l1.val if l1 else 0
-      v2 = l2.val if l2 else 0
+      l1_num = l1.val if l1 else 0
+      l2_num = l2.val if l2 else 0
+      sum_num = l1_num + l2_num + carry
+      new_num = sum_num % 10
+      carry = sum_num // 10
+      head.next = ListNode(new_num)
+      head = head.next
       
-      val = v1 + v2 + carry
-      carry =  val // 10
-      val = val % 10
-      curr.next = ListNode(val)
-      
+      # l1たちの更新
       l1 = l1.next if l1 else None
       l2 = l2.next if l2 else None
-      curr = curr.next
     return dummy.next
