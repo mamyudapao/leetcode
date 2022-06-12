2373de38bb54c48bfa41eff22157e0cab18661d4
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-06-12 19:37:54 +0900

    Time: 49 ms (33.77%), Space: 13.8 MB (99.14%) - LeetHub

---
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        i , digit = 0, 0
+        for c in abbr:
+            if c.isdigit():
+                if digit == 0 and c == '0':
+                    return False
+                digit *= 10
+                digit += int(c)
+            else:
+                if digit:
+                    i += digit
+                    digit = 0
+                if i >= len(word) or word[i] != c:
+                    return False
+                i += 1
+        if digit:
+            i += digit
+
+        return i == len(word)
