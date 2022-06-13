3ae624101a2d9d9ef4c4589756dff0a222d1ef55
noshian kenichi <43975545+mamyudapao@users.noreply.github.com>
2022-06-13 09:38:23 +0900

    Time: 354 ms, Memory: 43.2 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,11 +1,9 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        def helper(left, right):
-          if left < right:
-            s[left], s[right] = s[right], s[left]
-            helper(left+1, right -1)
-            
-        helper(0, len(s)-1)
+  def reverseString(self, s):
+    def helper(s, left, right):
+      if left < right:
+        s[left], s[right] = s[right], s[left]
+        helper(s, left + 1, right - 1)
+    
+    helper(s, 0, len(s)-1)
+    return s
